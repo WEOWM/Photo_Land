d449cf7112c4e7662ac78d6b0fc975f5db395c30
WEOWM <basims0077@gmail.com>
2023-05-12 12:02:59 +0530

    bug fix

--- src/commponte/Carousel/CategoryCarousel.jsx
+++ src/commponte/Carousel/CategoryCarousel.jsx
@@ -1,54 +1,7 @@
-// import Carousel from "react-multi-carousel";
-// import "react-multi-carousel/lib/styles.css";
-// import React from "react";
+import React from "react";
 import ProductCard from "../card/ProductCard";
 import data from "../../util/data";
-// import "./carousel.css"
-// const CategoryCarousel = () => {
-//   const responsive = {
-//     desktop: {
-//       breakpoint: { max: 3000, min: 1024 },
-//       items: 3,
-//       slidesToSlide: 3, // optional, default to 1.
-//     },
-//     tablet: {
-//       breakpoint: { max: 1024, min: 464 },
-//       items: 2,
-//       slidesToSlide: 2, // optional, default to 1.
-//     },
-//     mobile: {
-//       breakpoint: { max: 464, min: 0 },
-//       items: 1,
-//     },
-//   };
-//   return (
-//     <>
-//       <Carousel
-//         swipeable={false}
-//         draggable={false}
-//         showDots={true}
-//         responsive={responsive}
-//         infinite={true}
-//         autoPlaySpeed={1000}
-//         keyBoardControl={true}
-//         customTransition="all 1.9"
-//         transitionDuration={1000}
-//         containerClass="carousel-container"
-//         dotListClass="custom-dot-list-style"
-//         itemClass="carousel-item-padding-40-px"
-//       >
-//         {/* reuse card */}
-//         {data.map((item, idx) => {
-//           return <ProductCard productData={item} key={idx} />;
-//         })}
-//       </Carousel>
-//     </>
-//   );
-// };
 
-// export default CategoryCarousel;
-
-import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -68,7 +21,7 @@ const CategoryCarousel = () => {
   };
 
   return (
-    <div className=" me-5" style={{padding:"4rem",overflow:"hidden"}}  >
+    <div className=" me-5" style={{ padding: "4rem", overflow: "hidden" }}>
       <Slider {...settings}>
         {/* reuse card */}
         {data.map((item, idx) => {
